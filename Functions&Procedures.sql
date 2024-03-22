3f6d69eabb36e3633938bd1bc12dc810b6c57918
adonaione <adonairomero@icloud.com>
2024-03-22 09:38:47 -0600

    fixed errors

--- Functions&Procedures.sql
+++ Functions&Procedures.sql
@@ -1,39 +1,55 @@
 --Create a Stored Procedure that will insert a new film into the film table with the following arguments: title, description, release_year, language_id, rental_duration, rental_rate, length, replace_cost, rating
 
-CREATE OR REPLACE PROCEDURE insert_new_film @title varchar, @description varchar, @release_year integer, @language_id integer, @length integer @replacement_cost integer, @rating varchar
+CREATE OR REPLACE PROCEDURE insert_new_film(
+	title varchar, 
+	description varchar, 
+	release_year integer, 
+	language_id integer, 
+	rental_duration integer, 
+	rental_rate numeric(4,2), 
+	length integer, 
+	replacement_cost NUMERIC(5,2), 
+	rating mpaa_rating
+)
 LANGUAGE plpgsql
 AS $$
 BEGIN 
 	INSERT INTO film(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating)
-	WHERE title = @title AND description = @description and release_year=@release_year and language_id=@language_id and rental_duration=@rental_duration and rental_rate=@rental_rate, length=@length and replacement_cost=@replacement_cost and rating=@rating
-	);
+	VALUES(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating);
 END;
 $$;
 
 
-EXEC insert_new_film @title = 'New Movie', @description = 'A movie that is new to the film list.', @release_year = '2024', @language_id = '1', @length = '117', @replacement_cost = '14.99', @rating = 'R'; 
+CALL insert_new_film('New Movie', 'A movie that is new to the film list.', 2024, 1, 7, 9.99, 117, 14, 'R'); 
+
+SELECT * FROM film ORDER BY film_id DESC;
+
+
 
 
 --2. Create a Stored Function that will take in a category_id and return the number of
 --films in that category
 
-CREATE OR REPLACE FUNCTION num_of_films(category_id integer)
+SELECT * 
+from film 
+ORDER BY film_id DESC;
+
+DROP FUNCTION num_of_films;
+
+CREATE OR REPLACE FUNCTION num_of_films(cat_id integer)
 RETURNS integer
 LANGUAGE plpgsql
 AS $$
-	DECLARE film_num integer;
+	DECLARE cat_count integer;
 BEGIN 
-	SELECT count(*)
-	FROM film f 
-	JOIN film_category fc 
-	ON f.film_id = fc.film_id 
-	GROUP BY fc.category_id
-	WHERE fc.category_id = category_id;
-RETURN film_num;
+	SELECT count(*) INTO cat_count
+	FROM film_category 
+	WHERE category_id = cat_id;
+	RETURN cat_count;
 END;
 $$;
 
-SELECT num_of_films('11');
+SELECT num_of_films(15);
 
 
 
